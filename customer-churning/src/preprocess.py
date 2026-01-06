0b9c64b3c3234c186c7d5b86c087e0ead57ec560
thawtar <mr.thaw.tar1990@gmail.com>
2026-01-06 16:37:32 +0900

    added back previous ML training session notebooks

--- customer-churning/src/preprocess.py
+++ customer-churning/src/preprocess.py
@@ -1,6 +1,11 @@
 import numpy as np
 import pandas as pd
 
+# Data preprocessing, splitting and standardization
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
 
 # File to load data
 from logging import Logger
